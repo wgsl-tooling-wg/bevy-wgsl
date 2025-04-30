bc27f6ed389967ec371f2bf42b2df2396919e411
Mathis Brossier <mathis.brossier@gmail.com>
2025-04-30 02:05:15 +0200

    minor

--- examples/consumer/src/main.rs
+++ examples/consumer/src/main.rs
@@ -1,10 +1,27 @@
+use std::collections::HashMap;
+
+use wesl::Feature;
+
 fn main() {
     let source = wesl::Wesl::new("src/shaders")
         .add_package(&bevy_wgsl::bevy::Mod)
+        .add_constants([
+            ("MAX_CASCADES_PER_LIGHT", 10.0),
+            ("MAX_DIRECTIONAL_LIGHTS", 10.0),
+            ("PER_OBJECT_BUFFER_BATCH_SIZE", 10.0),
+        ])
         .set_options(wesl::CompileOptions {
             validate: true,
             lower: true,
-            // lazy: false,
+            lazy: false,
+            strip: false,
+            features: wesl::Features {
+                default: Feature::Enable,
+                flags: HashMap::from_iter([(
+                    "MESHLET_MESH_MATERIAL_PASS".to_string(),
+                    Feature::Disable,
+                )]),
+            },
             ..Default::default()
         })
         .compile("main")
